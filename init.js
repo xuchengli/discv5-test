fc02373df8fdbdd9262971080b551f8d39aaff70
xuchengli <lixucheng@aliyun.com>
2023-11-24 10:02:18 +0800

    create local enr with multiaddr

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import ip from 'ip';
 import { createSecp256k1PeerId } from '@libp2p/peer-id-factory';
+import { SignableENR } from "@chainsafe/discv5";
 import { toString } from 'uint8arrays/to-string';
+import { multiaddr } from "@multiformats/multiaddr";
 
 (async () => {
   const peerId = await createSecp256k1PeerId();
@@ -9,5 +12,9 @@ import { toString } from 'uint8arrays/to-string';
     id: toString(peerId.toBytes(), 'base58btc'),
     privKey: toString(peerId.privateKey, 'base64pad'),
     pubKey: toString(peerId.publicKey, 'base64pad'),
-  }));
+  }, null, 2));
+
+  const enr = SignableENR.createFromPeerId(peerId);
+  enr.setLocationMultiaddr(multiaddr(`/ip4/${ip.address()}/udp/5000`));
+  fs.writeFileSync('local-enr', enr.encodeTxt());
 })();
